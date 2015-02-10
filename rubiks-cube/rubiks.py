68a928cb84519e78be5ca8c13d1db7201b82efcd
suprita shankar <suprita.shankar@gmail.com>
2015-02-10 11:14:19 -0800

    initial instruction set

---
+++ rubiks-cube/rubiks.py
@@ -0,0 +1,11 @@
+"""
+	Accept an array of the different faces.
+	[6][9]
+	colors are: Red, Blue, Yellow, Green, White, Orange
+	[R,R,R,B,B,B,Y,B,O]
+	[Y,Y,R,B,W,W,G,G,Y]
+	ETC
+	Verify Number of R = No of Y = No of G ... = 9
+"""
+
+
